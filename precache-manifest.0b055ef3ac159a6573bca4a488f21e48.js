a671d36cd63f72aa4f36dc5379ed37c762b6efc5
Marius van der Busse <mariusvanderbusse@hotmail.com>
2020-02-26 23:00:07 +0200

    Updates

---
+++ precache-manifest.0b055ef3ac159a6573bca4a488f21e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a67e50c5ce50f1702940f5ad6e1d07",
+    "url": "/elisaViihde/index.html"
+  },
+  {
+    "revision": "21130fed19cd7babfe65",
+    "url": "/elisaViihde/static/css/main.7a8725aa.chunk.css"
+  },
+  {
+    "revision": "94c3d0babc9539c237d8",
+    "url": "/elisaViihde/static/js/2.44f1706c.chunk.js"
+  },
+  {
+    "revision": "5dedfc9e814c6faf0491be333adc943f",
+    "url": "/elisaViihde/static/js/2.44f1706c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21130fed19cd7babfe65",
+    "url": "/elisaViihde/static/js/main.a2437a5f.chunk.js"
+  },
+  {
+    "revision": "e6db5a752c1ad579548f",
+    "url": "/elisaViihde/static/js/runtime-main.727138d1.js"
+  }
+]);
